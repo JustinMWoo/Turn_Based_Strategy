12a886cfd0fe7648ca1d60db821c4bdbf19beaa3
Shahryar <shahryariqbal19@gmail.com>
2020-07-26 21:14:37 -0400

    First commit

---
+++ TurnBasedTile/Assets/Scripts/Character Classes/BaseCharacterClass.cs
@@ -0,0 +1,66 @@
+﻿
+using UnityEngine;
+
+public class BaseCharacterClass : MonoBehaviour
+{
+    private string CharacterClassName;
+    private string CharacterClassDescription;
+
+    // Stats
+    private characterStats Strength;
+    private characterStats Speed;
+    private characterStats Intellect;
+    private characterStats Health;
+    private characterStats Mana;
+    private characterStats Dexterity;
+
+
+    // can use get to read, or set to write value of names/stats
+    public string characterClassName
+    {
+        get { return CharacterClassName; }
+        set { CharacterClassName = value; }
+    }
+
+    public string characterClassDescription
+    {
+        get { return CharacterClassDescription; }
+        set { CharacterClassDescription = value; }
+    }
+
+    public characterStats strength
+    {
+        get { return Strength; }
+        set { Strength = value; }
+    }
+
+    public characterStats speed
+    {
+        get { return Speed; }
+        set { Speed = value; }
+    }
+
+    public characterStats intellect
+    {
+        get { return Intellect; }
+        set { Intellect = value; }
+    }
+
+    public characterStats health
+    {
+        get { return Health; }
+        set { Health = value; }
+    }
+
+    public characterStats mana
+    {
+        get { return Mana; }
+        set { Mana = value; }
+    }
+
+    public characterStats dexterity
+    {
+        get { return Dexterity; }
+        set { Dexterity = value; }
+    }
+}
---
+++ TurnBasedTile/Assets/Scripts/Character Classes/ThiefClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefClass : BaseCharacterClass
+{
+    public void thiefClass()
+    {
+        characterClassName = "Thief";
+        characterClassDescription = "Sneaky boi";
+        strength = new characterStats(20);
+        speed = new characterStats(10);
+        intellect = new characterStats(5);
+        health = new characterStats(50);
+        mana = new characterStats(20);
+        dexterity = new characterStats(10);
+    }
+}
